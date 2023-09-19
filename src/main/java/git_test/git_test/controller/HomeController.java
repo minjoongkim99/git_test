ca087c5717c22f82b7c2019080e49bc562f89773
alswndcjswo <alswndcjswo@gmail.com>
2023-09-19 15:56:25 +0900

    modify 1

---
+++ src/main/java/git_test/git_test/controller/HomeController.java
@@ -0,0 +1,15 @@
+package git_test.git_test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home(){
+        return "git_home";
+    }
+}
