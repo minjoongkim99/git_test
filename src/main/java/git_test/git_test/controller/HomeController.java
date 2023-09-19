52f65afa9904e2e19246252df49bba9b7befce3d
SsunyR <chrryblsm77@gmail.com>
2023-09-19 19:13:44 +0900

    First commit with intelliJ

--- src/main/java/git_test/git_test/controller/HomeController.java
+++ src/main/java/git_test/git_test/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String home(){
-        return "Collaborate with git";
+        return "Cowork with git";
     }
 }
